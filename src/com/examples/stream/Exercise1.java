d3b42e32274031b619a400fa5de96647a394eff8
Swastik Panda <swastiksuman@gmail.com>
2023-05-06 23:25:36 +0530

    adding reactive learnings

--- src/com/examples/stream/Exercise1.java
+++ src/com/examples/stream/Exercise1.java
@@ -7,23 +7,29 @@ public class Exercise1 {
         // Use StreamSources.intNumbersStream() and StreamSources.userStream()
 
         // Print all numbers in the intNumbersStream stream
-        ReactiveSources.intNumbersFlux().toStream().forEach(num -> System.out.println(num));
+        StreamSources.intNumbersStream().forEach(System.out::println);
 
         // Print numbers from intNumbersStream that are less than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream().filter(num -> num < 5).forEach(System.out::println);
 
         // Print the second and third numbers in intNumbersStream that's greater than 5
-        // TODO: Write code here
+        StreamSources.intNumbersStream().filter(number -> number > 5).skip(1).limit(2)
+        	.forEach(System.out::println);
 
         //  Print the first number in intNumbersStream that's greater than 5.
         //  If nothing is found, print -1
-        // TODO: Write code here
+        Integer value = StreamSources.intNumbersStream().filter(number -> number > 5).findFirst().orElse(-1);
+        System.out.println(value);
 
         // Print first names of all users in userStream
-        // TODO: Write code here
+        StreamSources.userStream().map(user -> user.getFirstName())
+        	.forEach(System.out::println);
 
         // Print first names in userStream for users that have IDs from number stream
-        // TODO: Write code here
+        StreamSources.intNumbersStream()
+        	.flatMap(id -> StreamSources.userStream().filter(user -> user.getId() == id))
+        	.map(user->user.getFirstName())
+        	.forEach(System.out::println);
 
     }
 
