9f7dad3c595611111246e27e3357102e58d0f6ae
Swastik Panda <swastiksuman@gmail.com>
2023-05-13 23:03:07 +0530

    adding reactive learnings

---
+++ src/com/examples/stream/Excercise2.java
@@ -0,0 +1,19 @@
+package com.examples.stream;
+
+public class Exercise2 {
+
+    public static void main(String[] args) throws IOException {
+
+        // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
+
+        // Print all numbers in the ReactiveSources.intNumbersFlux stream
+        // TODO: Write code here
+
+        // Print all users in the ReactiveSources.userFlux stream
+        // TODO: Write code here
+
+        System.out.println("Press a key to end");
+        System.in.read();
+    }
+
+}
