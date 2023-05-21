47b43ffd583849699359d8e213c9b0f9a994f2b3
Swastik Panda <swastiksuman@gmail.com>
2023-05-21 22:46:38 +0530

    adding reactive learnings

--- src/com/examples/stream/Excercise2.java
+++ src/com/examples/stream/Excercise2.java
@@ -1,14 +1,16 @@
 package com.examples.stream;
 
-public class Exercise2 {
+import java.io.IOException;
 
-    public static void main(String[] args) throws IOException {
+public class Excercise2 {
+
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         // Use ReactiveSources.intNumbersFlux() and ReactiveSources.userFlux()
-
+    	ReactiveSources.intNumbersFlux().subscribe(System.out::println);
         // Print all numbers in the ReactiveSources.intNumbersFlux stream
-        // TODO: Write code here
-
+        ReactiveSources.userFlux().subscribe(System.out::println);
+    	//Thread.sleep(5000);
         // Print all users in the ReactiveSources.userFlux stream
         // TODO: Write code here
 
